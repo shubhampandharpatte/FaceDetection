54e0a2908618a95baa95206d9ef02a0ddd9ff568
shubhampandharpatte <112811478+shubhampandharpatte@users.noreply.github.com>
2023-12-14 11:52:28 +0530

    Add files via upload

---
+++ FaceDetection.py
@@ -0,0 +1,24 @@
+import pathlib
+import cv2 as cv
+cascade_path=pathlib.Path(cv.__file__).parent.absolute()/"data/haarcascade_frontalface_default.xml"
+print(cascade_path)
+clf=cv.CascadeClassifier(str(cascade_path))
+camera=cv.VideoCapture("People.mp4")
+
+while True:
+    _,frame = camera.read()
+    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    faces= clf.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv.CASCADE_SCALE_IMAGE
+    )
+    for (x,y,width,height) in faces:
+        cv.rectangle(frame,(x,y),(x+width,y+height),(255,255,0),2)
+    cv.imshow("FACES",frame)
+    if cv.waitKey(0)==ord("q"):
+        break
+camera.release()
+cv.destroyAllWindows()
